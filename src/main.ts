174515829f7cc4e4eb74f4b187cb9a42f9891bc3
Samiiii1990 <sami.hermana@gmail.com>
2024-10-27 17:01:36 -0300

    login ok

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';  
 
-dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Habilitar CORS
+  app.enableCors({
+    origin: 'http://localhost:4200', // Cambia esto al origen de tu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type,Authorization',
+  });
+
   await app.listen(3000);
 }
 bootstrap();

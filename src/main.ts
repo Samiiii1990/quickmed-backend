c0c4a89d1c05112e5576fd204953d7500ffb7a01
Samiiii1990 <sami.hermana@gmail.com>
2024-12-09 23:56:14 -0300

    change url fe

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: 'https://quickmed-d91e5.web.app/',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: 'Content-Type,Authorization',
